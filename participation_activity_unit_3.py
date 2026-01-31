9772ce3598b598712d1bb9376df9d35c0dc7fbf8
Abdalla Farah <yamaani309@gmail.com>
2026-01-30 23:45:07 -0500

    add_file_comment

--- participation_activity_unit_3.py
+++ participation_activity_unit_3.py
@@ -1,3 +1,8 @@
+""" this program generates random names by combining names and adjectives from two lists.
+author: Abdalla Farah
+Date: 01/30/2026
+    
+"""
 import random
 
 names: list[str]=["grugur", "kouvok", "lethik", "broulos", "kaugan", "chaelazar", "raelazar", "dacular", "pazhar", "drukras"]
